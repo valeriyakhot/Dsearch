67cae7736b25d72032c409e0d61b551baa3a3eb8
valeria <valeria.ho66@leobaeck.net>
2017-04-20 09:47:33 +0300

    multicast

--- common/http_util.py
+++ common/http_util.py
@@ -1,16 +1,79 @@
 # -*- coding: utf-8 -*-
 
 import contextlib
+import datetime
 import errno
 import os
 import socket
+import struct
+import sys
 import traceback
 import urlparse
 
 from . import constants
-from . import util
 from . import send_it
+from . import util
+from . import xml_func
 
+MYPORT = 8123
+MYGROUP_4 = '225.0.0.250'
+MYTTL = 1 
+def node_in(nodes, ip, t) :
+    length=len(nodes)
+    for n in range(length):
+        if nodes[n]['ip'] == ip:
+            nodes[n]['lasttime']=t.strftime('%M:%S')
+            return True
+    return False
+    
+    
+def listener():
+    nodes=[]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    s.bind(('', MYPORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MYGROUP_4), socket.INADDR_ANY)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    s.settimeout(2)
+    while True:
+        print 'stuck in receive'
+        try:
+            data, sender = s.recvfrom(1500)
+            print 'SENDER'+ str(sender[0])+str(sender[1])
+            if not data:
+                break
+            while data[-1:] == '\0': data = data[:-1]
+            data=data.split(',')
+            node_list(sender, nodes)
+        except socket.timeout as e:
+            print e
+            break
+    print 'done'
+
+        
+    
+def sub_nodes(nodes):
+    t=datetime.datetime.now().time()
+    for i in range(len(nodes)):
+        time=nodes[i].get('lasttime').split(':')
+        sec=int(time[0])*60+int(time[1])
+        now=int(t.strftime('%M'))*60+int(t.strftime('%S'))
+        if (now-sec)>=10:
+            print ('len before'+str(len(nodes)))
+            print i
+            nodes.pop(i)
+            i-=1
+
+
+def node_list(data,nodes):
+    t=datetime.datetime.now().time()
+    if not node_in(nodes, data[0], t):
+        nodes.append({'ip': data[0], 'port': data[1], 'lasttime':t.strftime('%M:%S')})     
+    sub_nodes(nodes)
+    return nodes
+        
 
 def check(args, s, rest):
     req, rest = util.recv_line(s, rest)
@@ -42,7 +105,6 @@ def build_message(s,dic):
     headers = dic.get('headers', {})
     if 'content' in dic:
         dic['headers']['Content-Length']=len(dic.get('content'))
-    
     try:
         f=None
         if 'file_name' in dic:
@@ -67,11 +129,13 @@ def build_message(s,dic):
                 buf = f.read(constants.BLOCK_SIZE)
                 if not buf:
                     break
+                print buf
                 util.send_all(s,buf)
         else:
             util.send_all(s,dic.get('content'))
     finally:
-        f.close()
+        if 'file_name' in dic:
+            f.close()
 
 
 def server(args, func, mem=None):
@@ -114,6 +178,7 @@ def server(args, func, mem=None):
                             }
                             
                             build_message(status)
+                            print 'work'
                         else:
                             code=500
                             message= 'Internal Error'
@@ -140,3 +205,94 @@ def server(args, func, mem=None):
                             'content': 'Error %s %s\r\n%s'%(code,message,e),
                         }
                         build_message(status)
+
+def client (args, uri_beg, search, nodes):
+    output=[]
+    for node in nodes:
+        print 'NODE     %s'%(node)
+        with contextlib.closing(
+            socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+            )
+        ) as s:
+            s.connect((
+                nodes[node].get('ip', '127.0.0.1'),
+                nodes[node].get('port', constants.DEFAULT_HTTP_PORT),
+            ))
+            uri = uri_beg+search
+            util.send_all(
+                s,
+                (
+                    (
+                        'GET %s HTTP/1.1\r\n'
+                        'Host: %s\r\n'
+                        '\r\n'
+                    ) % (
+                        uri,
+                        args.url+uri,
+                    )
+                ).encode('utf-8'),
+            )
+
+            rest = bytearray()
+
+            #
+            # Parse status line
+            #
+            status, rest = util.recv_line(s, rest)
+            status_comps = status.split(' ', 2)
+            if status_comps[0] != constants.HTTP_SIGNATURE:
+                raise RuntimeError('Not HTTP protocol')
+            if len(status_comps) != 3:
+                raise RuntimeError('Incomplete HTTP protocol')
+
+            signature, code, message = status_comps
+            if code != '200':
+                raise RuntimeError('HTTP failure %s: %s' % (code, message))
+
+            content_length = None
+            for i in range(constants.MAX_NUMBER_OF_HEADERS):
+                line, rest = util.recv_line(s, rest)
+                if not line:
+                    break
+
+                name, value = util.parse_header(line)
+                if name == 'Content-Length':
+                    content_length = int(value)
+            else:
+                raise RuntimeError('Too many headers')
+
+            try:
+                if content_length is None:
+                    print 'IM HEEERRRREEE'
+                    buf = ''
+                    while True:
+                        buf += s.recv(constants.BLOCK_SIZE)
+                        if not buf:
+                            break
+                    
+                    output = buf
+
+                    return output
+                else:
+                    buff = ''
+
+                    left_to_read = content_length
+                    while left_to_read > 0:
+                        if not rest:
+                            t = s.recv(constants.BLOCK_SIZE)
+                            if not t:
+                                raise RuntimeError(
+                                    'Disconnected while waiting for content'
+                                )
+                            rest += t
+                        buf, rest = rest[:left_to_read], rest[left_to_read:]
+                        buff += buf
+                        left_to_read -= len(buf)
+
+                    output.append(buff)
+                print 'OUTPUT      %s'%output
+            finally:
+                pass
+    return output
