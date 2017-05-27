2c67894d5b8b05e7a690ee36071949c7bd4f7c1e
valeria <valeria.ho66@leobaeck.net>
2017-05-27 21:34:17 +0300

    moved

--- node/__main__.py
+++ node/__main__.py
@@ -7,15 +7,10 @@ import os
 #HOW TO RUN : python -m Dsearch.node --bind-port 8070
 
 from ..common import constants
-from ..common import xml_func
-from ..common import http_util
-from ..common import constants
 from ..common import util
-from ..common import xml_func
-from ..common import http_util
 from ..common import xml_funcs
 from ..common import httpServer
-from ..common import memory
+from . import memory
 from . import services
 
 
@@ -48,59 +43,6 @@ def parse_args():
     return args
 
 
-def node(s, uri, param, args, dic):
-    #print 'working'
-    if uri.startswith('/search?'):
-        files, ids = find_name(dic['memory'], param[0][0])
-        output = xml_func.xml_form(files, ids)
-        #print 'searching'
-
-        ret = {
-            'status': '200',
-            'message': 'OK',
-            'headers': {
-                'Content-Type': 'text/xml',
-            }, 
-            'content': output,
-        }
-        
-    elif uri.startswith('/get_file?'):
-        #print 'give file'
-        file_name = os.path.join(
-            dic['memory'][int(param[0][0])]['root'],
-            dic['memory'][int(param[0][0])]['filename'],
-        )
-        ret = {
-            'status': '200',
-            'message': 'OK',
-            'file_name': file_name,
-            'headers': {
-                'Content-Type': 'text/html',
-            }
-        }
-    else:
-        raise RuntimeError('Do not get known service')
-
-
-
-def mem_list(directory):
-    mem = []
-    for root, directories, filenames in os.walk(directory):
-        for filename in filenames:
-            mem.append({'root': root, 'filename': filename})
-    return mem
-
-
-def find_name(mem, name):
-    files = []
-    ids = []
-    for i in range(len(mem)):
-            if name in mem[i]['filename']:
-                files.append(mem[i]['filename'])
-                ids.append(i)
-    return files, ids
-
-
 def main():
     args = parse_args()
     Memory = memory.Memory(args)
@@ -108,12 +50,11 @@ def main():
     search = services.Search_service(mem)
     id = services.Id_service(mem)
     sender = services.Sender()
-    Server = httpServer.Http_server(args)
+    Server = httpServer.Http_server(args.bind_address, args.bind_port)
     Server.register('/get_file?', id)
     Server.register('/search?', search)
     Server.register('None?', sender)
     Server.server(Server)
-    #mem = mem_list(args.directory)
-    #http_util.server(args, node, http_util.sender, mem)
+
 if __name__ == '__main__':
     main()
--- node/services.py
+++ node/services.py
@@ -1,47 +1,37 @@
 
 import urlparse
 import socket
-
-import contextlib
-import datetime
-import errno
 import os
-import socket
-import struct
-import sys
-import traceback
-import urlparse
-MYPORT = 8123
-IP = '225.0.0.250'
-MYTTL = 1 
 
 from ..common import httpService
 from ..common import xml_funcs
 
-        
+MYPORT = 8123
+IP = '225.0.0.250'
+MYTTL = 1
+
+
 class Search_service (httpService.Http_service, object):
-    
+
     def __init__(self, memory):
         self.memory = memory
-        
-    def service (self, params, object):
-        pars_uri=urlparse.parse_qs(params['uri'][8:])
+
+    def service(self, params, object):
+        pars_uri = urlparse.parse_qs(params['uri'][8:])
         files, ids = object.find_name(self.memory, pars_uri.get('Search')[0])
         xml = xml_funcs.Xml()
         output = xml.xml_form(files, ids)
-        #print 'searching'
 
         ret = {
             'status': '200',
             'message': 'OK',
             'headers': {
                 'Content-Type': 'text/xml',
-            }, 
+            },
             'content': output,
         }
         return ret
-    
-    
+
     def find_name(self, mem, name):
         files = []
         ids = []
@@ -50,15 +40,15 @@ class Search_service (httpService.Http_service, object):
                 files.append(mem[i]['filename'])
                 ids.append(i)
         return files, ids
-    
-    
+
+
 class Id_service (httpService.Http_service, object):
 
     def __init__(self, memory):
         self.memory = memory
-        
-    def service (self, params, object):
-        pars_uri=urlparse.parse_qs(params['uri'][10:])
+
+    def service(self, params, object):
+        pars_uri = urlparse.parse_qs(params['uri'][10:])
         file_name = os.path.join(
             self.memory[int(pars_uri.get('id')[0])]['root'],
             self.memory[int(pars_uri.get('id')[0])]['filename'],
@@ -72,16 +62,15 @@ class Id_service (httpService.Http_service, object):
             }
         }
         return ret
-        
-class Sender (httpService.Http_service, object) :
+
+
+class Sender (httpService.Http_service, object):
 
     def __init__(self):
         pass
-        
+
     def service(self, params, object):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
-        s.sendto('node,%s'%(params['port']),(IP,MYPORT))
-        
-        
+        s.sendto('node,%s' % (params['port']), (IP, MYPORT))
