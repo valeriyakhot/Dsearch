131946f5c9ff0425429ca851474b288d331cce36
valeria <valeria.ho66@leobaeck.net>
2017-05-27 21:33:38 +0300

    .

--- front/__main__.py
+++ front/__main__.py
@@ -15,8 +15,6 @@ import xml.etree.cElementTree as ET
 
 from ..common import constants
 from ..common import util
-from ..common import xml_func
-from ..common import http_util
 from ..common import xml_funcs
 from ..common import httpServer
 from . import services
@@ -72,104 +70,13 @@ def parse_args():
     return parser.parse_args()
 
 
-def front(s, uri, param, args, dic):
-    nodes = dic['nodes']
-    # {
-        # '127.0.0.1:8040': {
-            # 'ip': '127.0.0.1',
-            # 'port': 8040,
-        # },
-        # '127.0.0.1:8070': {
-            # 'ip': '127.0.0.1',
-            # 'port': 8070,
-         # }
-     # }
-    if  nodes:
-        if uri.startswith(URI_SEARCH):
-            if len(uri) != len(URI_SEARCH):
-                out = http_util.client(args, URI_SEARCH, param[0][0], nodes)
-                output=HTML_TABLE_HEADER
-                for key in nodes:
-                    for o in out:
-                        output+=xml_func.xml_to_html(o, key)
-                output+='<a href="/form?file=search_form.html">&lt;Back&gt;</a>'+HTML_END
-            ret = {
-                'status': '200',
-                'message': 'OK',
-                'headers': {
-                    'Content-Type': 'text/html',
-                }, 
-                'content': output,
-            }
-        elif uri.startswith('/view_file?'):
-            pars_uri=urlparse.parse_qs(uri[11:])
-            node=pars_uri.get('node')[0]
-            id=pars_uri.get('id')[0]
-            #print "NODE   %s"%{str(node):nodes[str(node)]}
-            output = http_util.client(args, URI_ID, id, {str(node):nodes[str(node)]})
-            #print 'FRONT  OUTPUT    %s'%(output)
-            ret = {
-                'status': '200',
-                'message': 'OK',
-                'headers': {
-                    'Content-Type': 'text/html',
-                }, 
-                'content': str(output[0]),
-            }
-        elif uri.startswith('/download_file?'):
-            pars_uri=urlparse.parse_qs(uri[15:])
-            node=pars_uri.get('node')[0]
-            id=pars_uri.get('id')[0]
-            output = http_util.client(args, URI_ID, id, {str(node):nodes[str(node)]})
-            ret = {
-                'status': '200',
-                'message': 'OK',
-                'headers': {
-                    'Content-Disposition': 'attachment; filename=b.txt;',
-                    'Content-Type': 'text/html',
-                }, 
-                'content': str(output[0]),
-            }
-
-        elif uri.startswith('/form?'):
-            ret = {
-                'status': '200',
-                'message': 'OK',
-                'file_name':args.base%param[0][0],
-                'headers': {
-                    'Content-Type': 'text/html',
-                }
-            }
-        else:
-            ret={
-                'code': 404,
-                'message': 'File not found',
-                'headers': {
-                 'Content-Type': 'text/plain',
-                },
-                'content': 'Error : not known service:%s'%(uri),
-            }
-    else:
-        ret={
-                'code':500,
-                'message': 'No search services',
-                'headers': {
-                 'Content-Type': 'text/plain',
-                },
-                'content': 'Error :No search services',
-            }
-
-   
-    return ret
-
-
 def main():
     args = parse_args()
-    Server = httpServer.Http_server(args)
-    download = services.Download_service(args)
-    view = services.View_service(args)
-    form = services.Form_service(args)
-    search = services.Search_service(args)
+    Server = httpServer.Http_server(args.bind_address, args.bind_port)
+    download = services.Download_service(args.url)
+    view = services.View_service(args.url)
+    form = services.Form_service(args.base)
+    search = services.Search_service(args.url)
     listener = services.Listener()
     Server.register('/view_file?', view)
     Server.register('/form?', form)
@@ -177,7 +84,6 @@ def main():
     Server.register('/search?', search)
     Server.register('None?', listener)
     Server.server(Server)
-    #http_util.server(args, front, http_util.listener)
 
 if __name__ == '__main__':
     main()
--- front/node_client.py
+++ front/node_client.py
@@ -1,39 +1,19 @@
 
-import contextlib
-import datetime
-import errno
-import os
-import socket
-import struct
-import sys
-import traceback
-import urlparse
-
-from ..common import constants
-from ..common import xml_func
-from ..common import http_util
-from ..common import constants
-from ..common import util
-from ..common import xml_func
-from ..common import http_util
-from ..common import xml_funcs
-from ..common import httpServer
-from ..common import memory
-from ..common import httpService
 from ..common import httpClient
+
 
 class Node_client(object):
 
     def __init__(self, nodes):
         self.nodes = nodes
-        
-        
-    def n_client (self, uri_beg, search, url):
+
+    def n_client(self, uri_beg, search, url):
         output = []
         for node in self.nodes:
             uri = uri_beg+search
-            Client = httpClient.Client(url, self.nodes[node].get('ip'), self.nodes[node].get('port'))
+            Client = httpClient.Client(
+                            url, self.nodes[node].get('ip'),
+                            self.nodes[node].get('port')
+            )
             output.append(Client.client(uri))
         return output
-            
-            
